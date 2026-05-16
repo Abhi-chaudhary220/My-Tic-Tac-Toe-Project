1516ab16b69062437f1ebe9d0dd4a2165dc699bb
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-05-16 18:30:04 +0530

    adding logic

--- App.js
+++ App.js
@@ -1,3 +1,24 @@
 const buttons = document.querySelectorAll(".btn");
 const resetBtn = document.querySelector(".Reset");
 
+let playerTurnO = true;
+
+const winningPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+btn.forEach(element => {"click" () => {
+    if (playerTurnO) {
+        element.innerText = "x";
+        playerTurnO = false;
+    }
+}
+    
+});
